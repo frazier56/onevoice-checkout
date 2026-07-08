ad0d05700a2aa4cf9717c74a6aaecda09bb3990a
Lee Frazier <frazierlee@gmail.com>
2026-07-08 00:49:54 -0500

    Delete onevoice-checkout/api/debug-provision.js

--- onevoice-checkout/api/debug-provision.js
+++
@@ -1,5 +0,0 @@
-/* DISABLED. This endpoint previously created sub-accounts/users for debugging.
-   It has been neutralized for security and is safe to delete entirely. */
-export default async function handler(req, res) {
-  return res.status(410).json({ error: 'gone: endpoint disabled' });
-}
